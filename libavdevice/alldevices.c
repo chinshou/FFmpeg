986af912b7336335ab241dd09d591f24e71595d6
chinshou <chinshou1976@gmail.com>
2013-07-12 21:53:49 +0800

    fix some filters and devices and configure when upgrade to 2.0

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -51,6 +51,7 @@ void avdevice_register_all(void)
     REGISTER_OUTDEV  (CACA,             caca);
     REGISTER_INDEV   (DSHOW,            dshow);
     REGISTER_INDEV   (DV1394,           dv1394);
+    REGISTER_INDEV    (GDI, gdi);
     REGISTER_INDEV   (FBDEV,            fbdev);
     REGISTER_INDEV   (IEC61883,         iec61883);
     REGISTER_INDEV   (JACK,             jack);
@@ -65,6 +66,8 @@ void avdevice_register_all(void)
     REGISTER_INDEV   (VFWCAP,           vfwcap);
     REGISTER_INDEV   (X11GRAB,          x11grab);
     REGISTER_OUTDEV  (XV,               xv);
+
+    REGISTER_INDEV    (WAVEFORM, waveform);
 
     /* external libraries */
     REGISTER_INDEV   (LIBCDIO,          libcdio);
