3a0a7b3f25a3cd12efe9f150d1a90d6fe8435478
chinshou <chinshou1976@gmail.com>
2014-04-16 19:40:34 +0800

    fix build option

--- build/myconfigure_mac.sh
+++ build/myconfigure_mac.sh
@@ -1,6 +1,6 @@
 export PKG_CONFIG_LIBDIR=/usr/darwin32/lib/pkgconfig/
 export PKG_CONFIG_PATH=/usr/darwin32/lib/pkgconfig/
-LDFLAGS="-arch i386 -L/usr/lib -L/usr/darwin32/lib -static-libstdc++ -static-libgcc " CPPFLAGS="-arch i386 -I/usr/darwin32/include" CFLAGS="-arch i386 -I/usr/darwin32/include" ../configure --enable-memalign-hack --disable-outdev=sdl --arch=x86  \
+LDFLAGS="-arch i386 -L/usr/lib -L/usr/darwin32/lib " CPPFLAGS="-arch i386 -I/usr/darwin32/include" CFLAGS="-arch i386 -I/usr/darwin32/include" ../configure --enable-memalign-hack --disable-outdev=sdl --arch=x86  \
 --target-os=darwin --prefix=/usr/darwin32 --enable-gpl --disable-postproc --enable-shared  \
 --enable-cross-compile --enable-libx264 --enable-libvpx --disable-decoder=libvpx --enable-libgsm  \
 --enable-libmp3lame --enable-nonfree --disable-encoder=aac --enable-libfaac --enable-libvo-amrwbenc  \
