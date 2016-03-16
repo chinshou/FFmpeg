38d0db382f1eb281ef96e7f3d224298809c6fff8
chinshou <chinshou1976@gmail.com>
2016-03-16 22:34:59 +0800

    fix filter register

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -124,7 +124,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER(VIBRATO,        vibrato,        af);
     REGISTER_FILTER(VOLUME,         volume,         af);
     REGISTER_FILTER(VOLUMEDETECT,   volumedetect,   af);
-    //REGISTER_FILTER (SOX,         sox,         af);
+    REGISTER_FILTER (SOX,         sox,         af);
 
     REGISTER_FILTER(AEVALSRC,       aevalsrc,       asrc);
     REGISTER_FILTER(ANOISESRC,      anoisesrc,      asrc);
