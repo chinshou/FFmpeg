aed3df137e30bba945addccacecef62afc50efb9
chinshou <chinshou1976@gmail.com>
2016-11-23 11:33:32 +0800

    local changes

--- build/myconfigure_mac_decoder.sh
+++ build/myconfigure_mac_decoder.sh
@@ -1,22 +1,19 @@
-#!/bin/bash
 export PKG_CONFIG_LIBDIR=/usr/local/darwin32/lib/pkgconfig/
 export PKG_CONFIG_PATH=/usr/local/darwin32/lib/pkgconfig/
 LDFLAGS=" -L/usr/lib -L/usr/local/darwin32/lib " CPPFLAGS=" -I/usr/local/darwin32/include" CFLAGS=" -I/usr/local/darwin32/include" ../configure --enable-memalign-hack --disable-outdev=sdl --arch=x86  \
 --target-os=darwin --prefix=/usr/local/darwin32 --enable-gpl --disable-postproc --enable-shared  \
 --cc="cc -m32" \
---enable-cross-compile --enable-libx264 --enable-libvpx --disable-decoder=libvpx --enable-libgsm  \
---enable-libmp3lame --enable-nonfree --disable-encoder=aac --enable-libvo-amrwbenc  \
---enable-libfreetype --enable-version3  --enable-libtheora --enable-libspeex  \
---enable-libvorbis --enable-runtime-cpudetect --enable-libsox  --enable-libopencore-amrnb  \
---enable-libopencore-amrwb --enable-libxavs --enable-openal --enable-openssl --enable-librtmp  \
---enable-libx265   \
---enable-libwebp   \
---disable-muxer=encx   \
---disable-muxer=encx_audio   \
+--enable-cross-compile --disable-decoder=libvpx  \
+--enable-nonfree   \
+--enable-libfreetype --enable-version3  \
+--enable-runtime-cpudetect  \
+--enable-openal \
+--disable-muxers  \
+--disable-encoders   \
 --disable-demuxer=encx   \
---extra-ldflags=" -lcurl -liconv" \
+--extra-ldflags=" -liconv" \
 --extra-cflags=" -mmacosx-version-min=10.6 " --disable-indev=avfoundation \
- --enable-libopenjpeg  --enable-libilbc --enable-libass 
+--enable-libass 
 echo Please check libsdl existence problem , uninstall all libsdl
 read
 #--cc="clang -m32" --extra-cflags="-fomit-frame-pointer" --extra-ldflags="-fomit-frame-pointer" --extra-cxxflags="-fomit-frame-pointer"
@@ -27,6 +24,3 @@ read
 #--enable-libschroedinger
 #--enable-libvo-aacenc
 #--enable-libflite 
-#--enable-libbluray
-#--enable-libcelt
-#--enable-libxvid
