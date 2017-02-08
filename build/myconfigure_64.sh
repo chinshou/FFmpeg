4607c2ad26f3e0eb7228dcf932b723aa17217aaf
chinshou <chinshou1976@gmail.com>
2017-02-08 19:52:56 +0800

    remove dvdnav dvd read code
    remove dvdnav dvd read code

--- build/myconfigure_64.sh
+++ build/myconfigure_64.sh
@@ -1,10 +1,10 @@
 export PKG_CONFIG_PATH=/usr/x86_64-mingw32/lib/pkgconfig/
 ../configure --enable-memalign-hack --arch=x86 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32-  \
  --cc='x86_64-w64-mingw32-gcc' --disable-postproc --enable-shared --disable-static  \
- --disable-decoder=libvpx --disable-encoder=aac  \
+ --disable-decoder=libvpx \
  --enable-avisynth  --enable-gpl --enable-version3 --enable-pthreads --enable-avfilter \
  --enable-runtime-cpudetect --enable-nonfree --pkg-config=pkg-config \
- --enable-libfaac --enable-libnut --enable-libgsm --enable-libfreetype --enable-libvorbis --enable-libspeex \
+ --enable-libnut --enable-libgsm --enable-libfreetype --enable-libvorbis --enable-libspeex \
  --enable-libmp3lame --enable-zlib --enable-libtheora --enable-bzlib \
  --enable-libopencore-amrnb --enable-libopencore-amrwb \
  --enable-libx264 --enable-libbluray \
