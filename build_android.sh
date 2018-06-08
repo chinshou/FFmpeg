5a838abcb8a300a78ba8e5ae354ad77ce96cd405
chinshou <chinshou1976@gmail.com>
2018-06-08 12:49:37 +0900

    Fix some bug

--- build_android.sh
+++ build_android.sh
@@ -11,7 +11,7 @@ PREFIX=../android/$CPU
 ANDROID_SOURCE=./android-source
 ANDROID_LIBS=./android-libs
 ABI="armeabi-v7a"
-EXTRA_CFLAGS=" -I$PREFIX/include "
+EXTRA_CFLAGS=" -I$PREFIX/include -I/home/hubdog/Projects/android-ndk-r9/sources/android/arm/include"
 #EXTRA_CFLAGS="-I$ANDROID_SOURCE/frameworks/base/include -I$ANDROID_SOURCE/system/core/include"
 #EXTRA_CFLAGS="$EXTRA_CFLAGS -I$ANDROID_SOURCE/hardware/include"
 #EXTRA_CFLAGS="$EXTRA_CFLAGS -I$ANDROID_SOURCE/frameworks/base/media/libstagefright"
@@ -19,7 +19,7 @@ EXTRA_CFLAGS=" -I$PREFIX/include "
 #EXTRA_CFLAGS="$EXTRA_CFLAGS -I$NDK/sources/cxx-stl/gnu-libstdc++/4.8/include -I$NDK/sources/cxx-stl/gnu-libstdc++/4.8/libs/$ABI/include"
 
 EXTRA_CFLAGS="$EXTRA_CFLAGS -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
-EXTRA_LDFLAGS=" -fuse-ld=bfd -L$PREFIX/lib "
+EXTRA_LDFLAGS=" -fuse-ld=bfd -L$PREFIX/lib -L../$PREFIX/lib -lz -lm -logg"
 #EXTRA_LDFLAGS="-Wl,--fix-cortex-a8 -L$ANDROID_LIBS -Wl,-rpath-link,$ANDROID_LIBS -L$NDK/sources/cxx-stl/gnu-libstdc++/4.8/libs/$ABI"
 #EXTRA_CXXFLAGS="-Wno-multichar -fno-exceptions -fno-rtti"
 #    --disable-decoder=h264  \
@@ -28,31 +28,25 @@ function build_one
 {
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/
 ./configure \
-    --prefix=$PREFIX \
+    --prefix=$PREFIX --host-os=android \
     --enable-shared \
     --disable-static \
     --disable-doc \
     --disable-ffmpeg \
     --disable-ffplay \
     --disable-ffprobe \
-    --disable-ffserver \
     --enable-avdevice \
     --enable-nonfree \
-    --enable-libx264 \
-    --enable-libvorbis \
+    --enable-librtmp \
+    --enable-openssl \
     --enable-libfreetype \
     --enable-libspeex \
-    --enable-openssl \
     --enable-libgsm \
-    --enable-libmp3lame \
-    --enable-libsox \
-    --enable-librtmp \
-    --enable-libzvbi \
     --enable-jni \
     --enable-mediacodec \
-    --enable-libvidstab \
     --disable-decoder=libvpx \
     --enable-gpl \
+    --enable-libx264 \
     --disable-doc \
     --disable-symver \
     --disable-indev=dv1394 \
@@ -64,7 +58,7 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/
     --disable-decoder=h264_vdpau \
     --disable-muxer=encx \
     --disable-muxer=encx_audio \
-    --disable-demuxer=encx \
+    --enable-libmp3lame \
     --disable-indev=waveform  \
     --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
     --target-os=linux \
@@ -83,10 +77,22 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/
 #make
 #make install
 #    --disable-muxers \
+#     --disable-demuxer=encx \
 }
 
 ADDI_CFLAGS="-marm -march=armv7-a -mfpu=neon -mfloat-abi=softfp -mtune=cortex-a8"
 #ADDI_CFLAGS="-marm -march=armv7-a -mfpu=vfp -mfloat-abi=softfp "
 #ADDI_CFLAGS="-marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=softfp "
+#    
+#    --enable-libsox \
+#    
+#    --enable-libvidstab \
+#    --enable-libvorbis \
+#    
+#    
+#    
+#    
+#    --enable-libzvbi \
+#   
 
 build_one
